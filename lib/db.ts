5586f1756b907f1d283dfdfedf9da16d4b1dc287
Famesh Thakre <fameshthakre@users.noreply.github.com>
2026-07-31 17:49:44 +0530

    Fix TS error on pool type

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 declare global {
-  var __blueops_pg_pool: typeof import('pg').Pool | undefined;
+  var __blueops_pg_pool: import('pg').Pool | undefined;
 }
 
 export const pool = globalThis.__blueops_pg_pool ??= new Pool({
